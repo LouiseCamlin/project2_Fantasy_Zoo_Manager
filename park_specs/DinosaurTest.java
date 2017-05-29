b1a3a9b882a8c89b8f19ed6646923e165961ff2f
Louise Camlin <lacamlin@gmail.com>
2017-05-29 10:19:35 +0100

    added Dinosur tests

---
+++ park_specs/DinosaurTest.java
@@ -0,0 +1,37 @@
+import park_management.*;
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class DinosaurTest {
+
+  Dinosaur dinosaur;
+  TRex tRex;
+  Diplodocus diplodocus;
+  Plant plant;
+  Guest guest;
+
+  @Before
+  public void before(){
+    diplodocus = new Diplodocus("Lippy", 2, 5);
+    tRex = new TRex("Rexy", 10, 10);
+  }
+
+
+  @Test
+  public void canGetName(){
+    assertEquals("Lippy", diplodocus.getName());
+  }
+
+  @Test
+  public void canGetThreatLevel(){
+    assertEquals(2, diplodocus.getThreatLevel());
+  }
+
+  @Test
+  public void canGetHunger(){
+    assertEquals(10, tRex.getHunger());
+  }
+
+ 
+
+}
