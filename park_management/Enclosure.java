068663de89eca0ebcb7c412f1d384a5ca1450815
Louise Camlin <lacamlin@gmail.com>
2017-05-30 16:06:00 +0100

    only dinosaurs of a similar type can be added to enclosures

--- park_management/Enclosure.java
+++ park_management/Enclosure.java
@@ -5,20 +5,39 @@ import java.util.HashMap;
 public class Enclosure {
 
    private HashMap<String, Dinosaur> dinosaurs;
+   private String enclosureType;
 
    public Enclosure(){
     dinosaurs = new HashMap<String, Dinosaur>();
-   }
+}
 
-   public HashMap<String, Dinosaur> getDinosaurs(){
+public HashMap<String, Dinosaur> getDinosaurs(){
     return dinosaurs;
-   }
+}
 
-   public void addDinosaur(Dinosaur dinosaur){
-    dinosaurs.put(dinosaur.getName(), dinosaur);
-   }
+public int getNumberOfDinos() {
+    return dinosaurs.size();
+}
 
-   public void removeDinosaur(Dinosaur dinosaur){
+
+public void removeDinosaur(Dinosaur dinosaur){
     dinosaurs.remove(dinosaur.getName(), dinosaur);
-   }
+}
+
+public void addDinosaur(Dinosaur newDinosaur){
+
+    if(this.dinosaurs.size() == 0) {
+        dinosaurs.put(newDinosaur.getName(), newDinosaur);
+        enclosureType = newDinosaur.getClass().getName();
+        return;
+    }
+    if (newDinosaur.getClass().getName() == enclosureType) {
+        dinosaurs.put(newDinosaur.getName(), newDinosaur);
+    } else {
+        System.out.println("Only dinosaurs of a similar type can be added to an enclosure");
+    }
+
+}
+
 } 
+
--- park_specs/EnclosureTest.java
+++ park_specs/EnclosureTest.java
@@ -7,22 +7,35 @@ public class EnclosureTest {
   Enclosure enclosure;
   Dinosaur dinosaur;
 
+
   @Test
   public void canAddDinos(){
-  Dinosaur blue = new Velociraptor ("Blue", 10, 10);
-  Enclosure enclosure = new Enclosure();
-  enclosure.addDinosaur(blue);
-  assertEquals(1, enclosure.getDinosaurs().size());
+    Dinosaur blue = new Velociraptor ("Blue", 10, 10);
+    Enclosure enclosure = new Enclosure();
+    enclosure.addDinosaur(blue);
+    assertEquals(1, enclosure.getNumberOfDinos());
   }
 
   @Test
   public void canRemoveDinos(){
-  Dinosaur blue = new Velociraptor ("Blue", 10, 10);
-  Enclosure enclosure = new Enclosure();
-  enclosure.addDinosaur(blue);
-  enclosure.removeDinosaur(blue);
-  assertEquals(0, enclosure.getDinosaurs().size());
+    Dinosaur blue = new Velociraptor ("Blue", 10, 10);
+    Enclosure enclosure = new Enclosure();
+    enclosure.addDinosaur(blue);
+    enclosure.removeDinosaur(blue);
+    assertEquals(0, enclosure.getNumberOfDinos());
   }
+
+  @Test
+  public void canAddDinosAfterFirst() {
+    Enclosure enclosure = new Enclosure();
+    Dinosaur blue = new Velociraptor ("Blue", 10, 10);
+    Dinosaur alpha = new Velociraptor ("Alpha", 10, 10);
+    enclosure.addDinosaur(blue);
+    enclosure.addDinosaur(alpha);
+    assertEquals(2, enclosure.getNumberOfDinos());
+  }
+
+  
 
 
 }
